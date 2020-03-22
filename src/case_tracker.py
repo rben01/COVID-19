6b6db924605d2a5b4052c92246e3392b30695b87
Robert Bennett <rltbennett@icloud.com>
2020-03-22 11:01:46 -0400

    Added comments; changed how hue_order is calculated

--- src/case_tracker.py
+++ src/case_tracker.py
@@ -13,9 +13,8 @@ from matplotlib.ticker import LogLocator, NullFormatter, ScalarFormatter
 ROOT_PATH = Path("..")
 DATA_PATH = ROOT_PATH / "csse_covid_19_data" / "csse_covid_19_time_series"
 
-_STATE_COL = "Province/State"
-_COUNTRY_COL = "Country/Region"
-_SORT_ORDER_COL = "Sort Order"
+STATE_COL = "Province/State"
+COUNTRY_COL = "Country/Region"
 LOCATION_NAME_COL = "Location"
 DATE_COL = "Date"
 CASES_COL = "Cases"
@@ -25,30 +24,55 @@ CASE_TYPE_COL = "Case Type"
 def plot(df, style=None):
     style = style or "default"
     with plt.style.context(style):
+        # Order locations by decreasing current confirmed case count
+        # This is used to keep plot legend in sync with the order of lines on the graph
+        # so the location with the most cases is first in the legend and the least is
+        # the last
+        hue_order = (
+            df.groupby(LOCATION_NAME_COL)
+            .apply(
+                lambda g: pd.Series(
+                    {
+                        LOCATION_NAME_COL: g.name,
+                        CASES_COL: g.loc[
+                            g[CASE_TYPE_COL] == "Confirmed", CASES_COL
+                        ].iloc[-1],
+                    }
+                )
+            )
+            .sort_values(CASES_COL, ascending=False)[LOCATION_NAME_COL]
+        )
+
         g = sns.lineplot(
             x=DATE_COL,
             y=CASES_COL,
             data=df,
             hue=LOCATION_NAME_COL,
+            hue_order=hue_order,
             # palette="husl",
             style=CASE_TYPE_COL,
             style_order=["Confirmed", "Recovered", "Deaths"],
         )
-        plt.setp(g.lines, linewidth=3)
-        plt.xticks(rotation=90)
+
+        # Y axis setup
         plt.ylim(ymin=1)
         plt.yscale("log", basey=2, nonposy="mask")
 
+        # Configure ticks
         ax = plt.gca()
         ax: plt.Axes
         ax.xaxis.set_minor_locator(DayLocator())
         ax.yaxis.set_major_locator(LogLocator(base=2, numticks=1000))
         ax.yaxis.set_major_formatter(ScalarFormatter())
         ax.yaxis.set_minor_locator(
-            LogLocator(base=2, subs=np.linspace(0.5, 1, 6`), numticks=1000)
+            # 6-2 = 4 minor ticks between each pair of major ticks
+            LogLocator(base=2, subs=np.linspace(0.5, 1, 6)[1:-1], numticks=1000)
         )
         ax.yaxis.set_minor_formatter(NullFormatter())
 
+        # Configure design
+        plt.setp(g.lines, linewidth=3)
+        plt.xticks(rotation=90)
         plt.grid(b=True, which="both", axis="y")
         plt.grid(b=True, which="both", axis="x")
         plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
@@ -60,7 +84,7 @@ def get_df(filepath: Path, *, case_type: str):
     df = pd.read_csv(filepath)
     df: pd.DataFrame
     df = df.melt(
-        id_vars=[_STATE_COL, _COUNTRY_COL, "Lat", "Long"],
+        id_vars=[STATE_COL, COUNTRY_COL, "Lat", "Long"],
         var_name=DATE_COL,
         value_name=CASES_COL,
     )
@@ -79,16 +103,24 @@ def join_dfs():
         dfs.append(df)
 
     df = pd.concat(dfs, axis=0)
+
+    # Aggregate cities in China
     china = (
-        df[df[_COUNTRY_COL] == "China"]
+        df[df[COUNTRY_COL] == "China"]
         .groupby([DATE_COL, CASE_TYPE_COL], as_index=False)
         .agg({"Lat": "first", "Long": "first", CASES_COL: "sum"})
     )
-    china[_COUNTRY_COL] = "China"
+    china[COUNTRY_COL] = "China"
     df = pd.concat([df, china], axis=0)
 
-    df[_COUNTRY_COL] = df[_COUNTRY_COL].replace("Korea, South", "South Korea")
-    df[LOCATION_NAME_COL] = df[_STATE_COL].fillna(df[_COUNTRY_COL])
+    df[COUNTRY_COL] = df[COUNTRY_COL].replace("Korea, South", "South Korea")
+
+    # Use state as location name for states, else use country name
+    df[LOCATION_NAME_COL] = df[STATE_COL].fillna(df[COUNTRY_COL])
+
+    # Hereafter df is sorted by date, which is helpful as it allows using .iloc[-1]
+    # to get current (or most recent known) situation
+    df = df.sort_values([LOCATION_NAME_COL, DATE_COL])
     return df
 
 
@@ -104,39 +136,29 @@ INCLUDED_COUNTRIES = [
 
 
 df = df[
-    df[_COUNTRY_COL].isin(INCLUDED_COUNTRIES)
-    & ~((df[_COUNTRY_COL] == "China") & (df[_STATE_COL].notna()))
-    & (~df[_STATE_COL].str.contains(",").fillna(False))
+    df[COUNTRY_COL].isin(INCLUDED_COUNTRIES)
+    & ~((df[COUNTRY_COL] == "China") & (df[STATE_COL].notna()))
+    & (~df[STATE_COL].str.contains(",").fillna(False))
     & (df[DATE_COL] >= pd.to_datetime("2020-02-20"))
 ]
 
 
 df = df.groupby(LOCATION_NAME_COL).filter(
-    lambda g: g[_COUNTRY_COL].iloc[0] not in ["US", "United Kingdom"]
+    # Exclude US states (unless they meet certain criteria, see below) and
+    # UK's discontiguous regions (e.g., Isle of Man, Gibraltar)
+    lambda g: g[COUNTRY_COL].iloc[0] not in ["US", "United Kingdom"]
+    # Keep UK the country
     or (g.name == "United Kingdom")
     or (
-        g.loc[g[CASE_TYPE_COL] == "Confirmed", CASES_COL].max()
-        >= df[df[_COUNTRY_COL] == "US"]
+        # Keep top n US states by current number of confirmed cases
+        g.loc[g[CASE_TYPE_COL] == "Confirmed", CASES_COL].iloc[-1]
+        >= df[df[COUNTRY_COL] == "US"]
         .groupby(LOCATION_NAME_COL)
-        .apply(lambda h: h.loc[h[CASE_TYPE_COL] == "Confirmed", CASES_COL].max())
+        .apply(lambda h: h.loc[h[CASE_TYPE_COL] == "Confirmed", CASES_COL].iloc[-1])
         .nlargest(7)
         .iloc[-1]
     )
 )
-
-df = df.merge(
-    df.groupby(LOCATION_NAME_COL)
-    .apply(
-        lambda g: g[g[CASE_TYPE_COL] == "Confirmed"]
-        .sort_values(DATE_COL)[CASES_COL]
-        .iloc[-1]
-    )
-    .rename(_SORT_ORDER_COL),
-    on=LOCATION_NAME_COL,
-    how="left",
-)
-
-df = df.sort_values(_SORT_ORDER_COL, ascending=False)
 
 plot(df)
 plt.show()
